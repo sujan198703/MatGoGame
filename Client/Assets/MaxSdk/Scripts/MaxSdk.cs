c0b5a311c644e4074b9ed2e29d055364054090ab
rajaharris55 <rajaharris55@gmail.com>
2022-07-25 11:39:29 +0500

    Game events

--- Client/Assets/MaxSdk/Scripts/MaxSdk.cs
+++ Client/Assets/MaxSdk/Scripts/MaxSdk.cs
@@ -18,7 +18,7 @@ public class MaxSdk :
     MaxSdkUnityEditor
 #endif
 {
-    private const string _version = "5.4.4";
+    private const string _version = "5.4.5";
 
     /// <summary>
     /// Returns the current plugin version.
